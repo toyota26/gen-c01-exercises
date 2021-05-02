a5a820e42d8541a8c79837726b9062ad9cae6845
toyota26 <80878393+toyota26@users.noreply.github.com>
2021-05-03 01:18:11 +0800

    JS-1.2

---
+++ Desktop/gen-c01-exercises/JS-1.2-Dog Years/app.js
@@ -0,0 +1,10 @@
+let myAge = 27;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = "Toyota".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
